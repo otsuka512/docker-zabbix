457ca4ff078ff4fc28184e09725f47978f73d1c1
Koichi Otsuka <otsuka@kubotek.co.jp>
2015-06-26 11:17:33 +0900

    Move volume directory to /srv/docker

--- backup.sh
+++ backup.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 datestr=`date +%Y%m%d`
-dockerdir=/home/otsuka/docker
-zbfile=$dockerdir/zabbix-$datestr.tar.bz2
+backupdir=$HOME/docker/zabbix/backup
 
 echo -n "Stop Zabbix Okey [y/n]?"
 read YN
@@ -10,9 +9,9 @@ case "$YN" in
 	docker exec -i -t zabbix monit -d 10 -c /etc/monitrc stop zabbix-server
 	echo -n "Confirm to Stop Zabbix-server [enter to continue]"
 	read YN
-	docker exec -i -t zabbix mysqldump -uroot -pmypassword --default-character-set=utf8 --database zabbix > $dockerdir/zabbix/backup/zabbix_dump_$datestr.db
+	docker exec -i -t zabbix mysqldump -uroot -pmypassword --default-character-set=utf8 --database zabbix > $backupdir/zabbix_dump_$datestr.db
 	docker exec -i -t zabbix monit -d 10 -c /etc/monitrc start zabbix-server
-	bzip2 $dockerdir/zabbix/backup/zabbix_dump_$datestr.db
+	bzip2 $backupdir/zabbix_dump_$datestr.db
 	;;
     *)
 	echo "nothing to do"
--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ docker run --restart=always -d \
  -p 10051:10051 \
  -p 10052:10052 \
  --name zabbix  \
- -v /home/otsuka/docker/zabbix/mysql:/var/lib/mysql \
- -v /home/otsuka/docker/zabbix/alertscripts:/usr/lib/zabbix/alertscripts \
- -v /home/otsuka/docker/zabbix/externalscripts:/usr/lib/zabbix/externalscripts \
- -v /home/otsuka/docker/zabbix/zabbix_agentd.d:/etc/zabbix/zabbix_agentd.d otsuka512/docker-zabbix
+ -v /srv/docker/zabbix/mysql:/var/lib/mysql \
+ -v /srv/docker/zabbix/alertscripts:/usr/lib/zabbix/alertscripts \
+ -v /srv/docker/zabbix/externalscripts:/usr/lib/zabbix/externalscripts \
+ -v /srv/docker/zabbix/zabbix_agentd.d:/etc/zabbix/zabbix_agentd.d otsuka512/docker-zabbix
